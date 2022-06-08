fbcbd2c5ee9cefd41314d79b5a8c9fe351afe678
CZX <rayudu.addagarla@economical.com>
2022-06-08 12:02:00 -0400

    initial version

---
+++ hello.py
@@ -0,0 +1,34 @@
+# This is way to comment in python
+"""
+This is my first program
+This is my 2nd line
+"""
+
+print ("Hello py Kids")
+print(2+3)
+x=100
+y=2.2
+z=9.9999
+name="Haris"
+
+apples,mangoes=10,20
+
+
+num_of_oranges=20
+
+# Do not start with number
+# dont use space
+# dont use hyphen-
+
+
+print("total is")
+print(apples+mangoes)
+def move(x):
+	print(x)
+
+if(x>50):
+	move(10)
+
+
+
+
