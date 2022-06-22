6a9b7635340a3cccfaeacdd400e4f7d43b54f14f
CZX <rayudu.addagarla@economical.com>
2022-06-21 20:02:56 -0400

    Initial version

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
 # variables
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
 
-x=10
-
-if(x=10):
-    print("hello")
-
-# operators
+"""
