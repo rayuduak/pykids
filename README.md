29c1f7b4a4678e9607188ea100b959daecdd8272
CZX <rayudu.addagarla@economical.com>
2022-06-29 10:37:37 -0400

    checking in readme and variables.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pykids
 python for kids
+
+1. hello.py
+2. variables.py
+3. learnstrings.py
+4. array_list.py
--- variables.py
+++ variables.py
@@ -1,8 +1,40 @@
 # variables
 """
+In python a variable is created when a value is assigned to it.
+example:  
+"""
+x = 2
+print(x)
+
+"""
 A variable name must start with a letter or the underscore character
 A variable name cannot start with a number
 A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 Variable names are case-sensitive (age, Age and AGE are three different variables)
+ """
+ 
+#  Correct examples: fruit, count_of_fruits, firstName, last_Name, Name2, _vegetables_
+#  Incorrect examples:   2lastname, first-last, middle name
 
-"""
+#Multiple assignments at one shot
+a, b, c = 10, 20, 30
+
+#global variables: declAre varible outside a def and use them inside and able to change the same variable inside def
+#example1:
+
+print("\nexample1: ")
+compliment = "You are amazing"
+def greeting():
+    print(compliment)
+greeting()
+
+
+#example2:
+print("\nexample2")
+compliment = "You are amazing"
+def greeting():
+    compliment= "You are superkid"
+    print(compliment)
+
+greeting()
+print(compliment)
