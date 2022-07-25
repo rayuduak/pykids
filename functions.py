7243495875d617e8dfc2d4be4cfd5b82a0b69a8d
CZX <rayudu.addagarla@economical.com>
2022-07-25 19:29:22 -0400

    checking in to git

--- functions.py
+++ functions.py
@@ -40,28 +40,3 @@ for student in students:
     display_Grade(student)
 
 
-#Bank.py
-#Create a Python banking system
-
-#Withdraw, Deposit , CheckBalance - functions
-#Balance 1000.00
-
-balance=1000.00
-
-def withdraw():
-    pass
-
-def deposit():
-    pass
-
-def checkBalance():
-    print(balance)
-
-checkBalance() # what will it print
-deposit(100)
-withdraw(50)
-deposit(11)
-deposit(20)
-# school, restaurant, shopping
-# mom giving moremoney, birthday uncle, friends give some money
-# Alert if balance is less than 100
