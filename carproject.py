3c9068cfadcd5167833a485c38b0a04fe07f9257
CZX <rayudu.addagarla@economical.com>
2022-08-15 16:15:32 -0400

    Car project if any doubts please ask on whats app or email

---
+++ carproject.py
@@ -0,0 +1,15 @@
+#Project
+"""
+Creat a Car class and add all methods a car can have
+example method properties are car make, model, color are attributes
+example add speed/accelerate, brake, start ,stop, lightsonoff, turn left right etc
+use global variable and use user inputs on a console application to interact with car
+display the car status of all this values as they change
+ask user to build a car of what they want and then as questions to increase speed or decrease speed
+turns ligths etc.
+At this point its all console but can improve by using pygame later
+
+Use the concepts taught so far
+Classes , Objects, Functions, Variables, Loops, Console input, Casting, Conditions, etc.
+
+"""
